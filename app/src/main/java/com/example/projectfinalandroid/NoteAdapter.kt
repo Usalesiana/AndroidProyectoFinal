2affa21802e942876908bf3cf9ae5bd41d2dde56
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2024-06-30 22:20:46 -0400

    Add adapter and layout

---
+++ app/src/main/java/com/example/projectfinalandroid/NoteAdapter.kt
@@ -0,0 +1,28 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projectfinalandroid.R
+import com.example.projectfinalandroid.models.Note
+
+class NoteAdapter(private val noteList: List<Note>) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvTitle: TextView = itemView.findViewById(R.id.tvTitle)
+        val tvDate: TextView = itemView.findViewById(R.id.tvDate)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false)
+        return NoteViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = noteList[position]
+        holder.tvTitle.text = currentNote.title
+        holder.tvDate.text = currentNote.dateCreated
+    }
+
+    override fun getItemCount() = noteList.size
+}
