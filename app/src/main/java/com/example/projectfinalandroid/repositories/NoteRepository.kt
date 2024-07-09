82e71339af5fe569a27a890bfad75cf7a3cb6693
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2024-07-09 00:27:35 -0400

    Restore NoteRepository

--- app/src/main/java/com/example/projectfinalandroid/repositories/NoteRepository.kt
+++ app/src/main/java/com/example/projectfinalandroid/repositories/NoteRepository.kt
@@ -1,16 +1,4 @@
 package com.example.projectfinalandroid.repositories
 
-import com.example.projectfinalandroid.api.NotesApiService
-import com.example.projectfinalandroid.api.LoginRequest
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import retrofit2.Response
-
-class NoteRepository(private val notesApiService: NotesApiService) {
-
-    fun login(username: String, password: String): Flow<Response<String>> = flow {
-        val loginRequest = LoginRequest(username, password)
-        val response = notesApiService.login(loginRequest).execute()
-        emit(response)
-    }
+class NoteRepository {
 }
